03812b14eb1c7447f170af4aa5bdac2027edcd56
Zibeyda3012 <musayevazibeyd@gmail.com>
2024-04-22 12:49:33 +0400

    last version

--- WpfApp_Calculator/MainWindow.xaml.cs
+++ WpfApp_Calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,47 @@ using System.Windows.Shapes;
 
 namespace WpfApp_Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void equality_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                var Result = dt.Compute(TextLabel.Content.ToString(), "");
+                TextLabel.Content = Result.ToString();
+            }
+            catch
+            {
+                TextLabel.Content = "";
+            }
+        }
+
+        private void clear_btn2_Click(object sender, RoutedEventArgs e)
+        {
+            TextLabel.Content = "";
+        }
+
+        private void clear_btn1_Click(object sender, RoutedEventArgs e)
+        {
+            TextLabel.Content = "";
+        }
+
+        private void btn_click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string text = TextLabel.Content.ToString();
+            if (text == "0")
+                text = "";
+            text += btn.Content;
+            TextLabel.Content = text;
         }
     }
 }
